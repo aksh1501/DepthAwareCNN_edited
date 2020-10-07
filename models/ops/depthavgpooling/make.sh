3917510c53d7469086d20dfadbbfd63d1ec58a64
akshit sharma <31561774+aksh1501@users.noreply.github.com>
2020-10-07 22:43:22 +0530

    Update make.sh

--- models/ops/depthavgpooling/make.sh
+++ models/ops/depthavgpooling/make.sh
@@ -1,4 +1,4 @@
 cd src
 nvcc -c -o depthavgpooling_cuda_kernel.cu.o depthavgpooling_cuda_kernel.cu -x cu -Xcompiler -fPIC -std=c++11 -arch=sm_60
 cd ..
-CC=g++ python build.py
+CC=g++ python src/setup.py install
